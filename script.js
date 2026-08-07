b04722f3e282d605e4948214c1dbe7c255e6884e
Ariel Andres Fuentes Muñoz <Ariel.Fuentes@telefonica.com>
2026-08-07 14:05:27 -0400

    v0.2 - Panel de recaudación

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Bienvenido a Cafetín");
+fetch("datos.json")
+.then(response => response.json())
+.then(datos => {
+
+    document.getElementById("titulo").textContent = datos.titulo;
+
+    document.getElementById("mensaje").textContent = datos.mensaje;
+
+    document.getElementById("meta").textContent =
+        "$" + datos.meta.toLocaleString("es-CL");
+
+    document.getElementById("recaudado").textContent =
+        "$" + datos.recaudado.toLocaleString("es-CL");
+
+    document.getElementById("fecha").textContent =
+        datos.ultimaActualizacion;
+
+    const porcentaje =
+        (datos.recaudado / datos.meta) * 100;
+
+    document.getElementById("porcentaje").textContent =
+        porcentaje.toFixed(1) + "%";
+
+    document.getElementById("progreso").style.width =
+        porcentaje + "%";
+
+});
