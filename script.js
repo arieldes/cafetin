e5048eb0e724efb9e21444d94324bd3c16ff7975
Ariel Andres Fuentes Muñoz <Ariel.Fuentes@telefonica.com>
2026-08-10 18:48:35 -0400

    Agrega estados de Cafetin segun avance de colecta

--- script.js
+++ script.js
@@ -18,6 +18,35 @@ fetch("datos.json")
     const porcentaje =
         (datos.recaudado / datos.meta) * 100;
 
+    const cafetin = document.getElementById("cafetin");
+
+    if (porcentaje < 25) {
+
+    cafetin.src = "assets/img/cafetin1.png";
+
+} else if (porcentaje < 50) {
+
+    cafetin.src = "assets/img/cafetin2.png";
+
+} else if (porcentaje < 75) {
+
+    cafetin.src = "assets/img/cafetin3.png";
+
+} else if (porcentaje < 100) {
+
+    cafetin.src = "assets/img/cafetin4.png";
+
+} else {
+
+    cafetin.src = "assets/img/cafetin5.png";
+
+}
+
+    const faltante = datos.meta - datos.recaudado;
+
+    document.getElementById("faltante").textContent =
+        "Nos faltan $" + faltante.toLocaleString("es-CL");
+
     document.getElementById("porcentaje").textContent =
         porcentaje.toFixed(1) + "%";
 
